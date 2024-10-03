a5d6d399eb21443d4944056250776b6be026bf66
Santiagof2 <santiagofritz97@gmail.com>
2024-10-03 20:24:27 -0300

    cambios en response de /auth/login

--- server/routes/auth.py
+++ server/routes/auth.py
@@ -9,7 +9,7 @@ auth_bp = Blueprint('auth_bp', __name__, url_prefix='/auth')
 def login():
     data = request.get_json()
 
-    if not (data.get('username') and data.get('password')): return jsonify({'error': 'Faltan datos.'})
+    if not (data.get('username') and data.get('password')): return jsonify({'error': 'Usuario o contraseña incorrectos.'}), 400
 
     for user in Database.users:
         if user._username == data.get('username') and user._password == data.get('password'): return jsonify({'mensaje': 'Usuario y contraseña correctos'}), 200
