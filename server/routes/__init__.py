c9074e3af2ce6467c38c57292c154e8cf5beda33
Alejandro Llontop <bite12221@gmail.com>
2024-10-24 11:10:05 -0300

    se agrego la ruta para que sea accesible desde otros archivos

--- server/routes/__init__.py
+++ server/routes/__init__.py
@@ -10,6 +10,6 @@ from server.routes.passenger_routes import passenger_bp
 from server.routes.message_routes import message_bp
 from server.routes.vehicle_driver_routes import vehicle_driver_bp
 from server.routes.passenger_trip_routes import passenger_trip_bp
+from server.routes.trip_filter_routes import trip_filter_bp
 
-
-__all__ = ['passenger_trip_bp', 'vehicle_driver_bp', 'user_bp', 'driver_bp', 'trip_join_bp', 'trip_bp', 'address_bp', 'auth_bp', 'role_selection','vehicle_bp', 'passenger_bp', 'message_bp']
+__all__ = ['passenger_trip_bp', 'vehicle_driver_bp', 'user_bp', 'driver_bp', 'trip_join_bp', 'trip_bp', 'address_bp', 'auth_bp', 'role_selection','vehicle_bp', 'passenger_bp', 'message_bp', 'trip_filter_bp',]
