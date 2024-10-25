e9e9135354b8d7e7da66afc8e4751c742585bbb3
xStranged <fede.valle04@gmail.com>
2024-10-24 23:41:01 -0300

    fix: :bug: fix login

--- server/routes/auth_routes.py
+++ server/routes/auth_routes.py
@@ -13,9 +13,8 @@ def login():
     if not (data.get('username') and data.get('password')): return jsonify({'error': 'Usuario o contraseña incorrectos.'}), 400
 
     user = User.query.filter_by(username=data.get('username')).first()
-
     if not user: return jsonify({'error': 'Usuario no encontrado.'}), 400
 
     if data['password'] != user.password: return jsonify({'error': 'Usuario o contraseña incorrectos.'}), 400
 
-    return jsonify({user.to_dict()}), 200
+    return jsonify(user.to_dict()), 200
