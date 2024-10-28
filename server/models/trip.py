1839814a06a40a257313a03dd15f43fbc017737a
Mateo Sanchez Viamonte <78273332+mateksv@users.noreply.github.com>
2024-10-28 11:32:57 -0300

    Fix id PassengerTrip

--- server/models/trip.py
+++ server/models/trip.py
@@ -12,7 +12,7 @@ class Trip(db.Model):
     departure_address_id = db.Column(db.Integer, db.ForeignKey('Address.id'), nullable=False)
     arrival_address_id = db.Column(db.Integer, db.ForeignKey('Address.id'), nullable=False)
     vehicle_driver_id = db.Column(db.Integer, db.ForeignKey('Vehicle_Driver.driver_id'), nullable=False)
-    status = db.Column(db.String(50), nullable=False, default='active') # Nuevo campo
+    status = db.Column(db.String(50), nullable=False, default='active')
 
     departure_address = db.relationship('Address', foreign_keys=[departure_address_id])
     arrival_address = db.relationship('Address', foreign_keys=[arrival_address_id])
