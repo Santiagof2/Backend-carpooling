a4d9f804806a1e240662f8327fac6f012fd705c9
ShiromaHajime <hajishiro.hs@gmail.com>
2024-10-28 15:43:52 -0300

    ruta de alta de viaje con alta de direcciones

--- server/routes/address_routes.py
+++ server/routes/address_routes.py
@@ -11,3 +11,31 @@ def get_address():
     addresses = Address.query.all()
     result = [address.to_dict() for address in addresses]
     return jsonify(result)
+
+# Crear una dirección
+@address_bp.route('/', methods=['POST'])
+def create_address():
+    data = request.get_json()
+
+    # Validar los datos necesarios
+    if not data or not all(key in data for key in ['street', 'number', 'latitude', 'longitude', 'locality_id']):
+        return jsonify({'message': 'Missing required fields'}), 400
+    
+    try:
+
+        # Crear una nueva dirección
+        new_address = Address(
+            street=data['street'],
+            number=data['number'],
+            latitude=data['latitude'],
+            longitude=data['longitude'],
+            locality_id=data['locality_id']
+        )
+
+        db.session.add(new_address)
+        db.session.commit()
+
+        return jsonify(new_address.to_dict()), 201
+
+    except Exception as e:
+        return jsonify
--- server/routes/trip_routes.py
+++ server/routes/trip_routes.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from pprint import pprint
 from flask import Blueprint, jsonify, request
-from server.models import Trip
+from server.models import Trip, Address, Locality, Principal_Subdivision
 from server.db import db
 
 trip_bp = Blueprint('trip_bp', __name__, url_prefix='/trips')
@@ -34,9 +34,32 @@ def create_trip():
     data = request.get_json()
 
     # Validar los datos necesarios
-    if not data or not all(key in data for key in ['departure_date', 'departure_time', 'available_seats', 'seat_price', 'departure_address_id', 'arrival_address_id', 'vehicle_driver_id']):
+    if not data or not all(key in data for key in ['departure_date', 'departure_time', 'available_seats', 'seat_price', 'vehicle_driver_id', 'departure_address', 'arrival_address']):
         return jsonify({'message': 'Missing required fields'}), 400
     
+    if not all(key in data['departure_address'] for key in ['street', 'number', 'latitude', 'longitude', 'locality_id']):
+        return jsonify({'message': 'Missing required fields in departure address'}), 400
+    
+    if not all(key in data['arrival_address'] for key in ['street', 'number', 'latitude', 'longitude', 'locality_id']):
+        return jsonify({'message': 'Missing required fields in arrival address'}), 400
+    
+
+    new_departure_address = Address(
+        street=data['departure_address']['street'],
+        number=data['departure_address']['number'],
+        latitude=data['departure_address']['latitude'],
+        longitude=data['departure_address']['longitude'],
+        locality_id=data['departure_address']['locality_id']
+    )
+
+    new_arrival_address = Address(
+        street=data['arrival_address']['street'],
+        number=data['arrival_address']['number'],
+        latitude=data['arrival_address']['latitude'],
+        longitude=data['arrival_address']['longitude'],
+        locality_id=data['arrival_address']['locality_id']
+    )
+
     try:
 
         # Obtener la fecha y hora actual
@@ -47,6 +70,14 @@ def create_trip():
         fromated_departure_date = datetime.strptime(data['departure_date'], '%Y-%m-%d')
         formated_departure_time = datetime.strptime(data['departure_time'], '%H:%M:%S').time()
         
+        # Agregar las nuevas direcciones a la base de datos
+        db.session.add(new_departure_address)
+        db.session.add(new_arrival_address)
+        db.session.commit()
+
+        # Obtener las direcciones de salida y llegada 
+        new_departure_address_id = Address.query.get(new_departure_address.id).id
+        new_arrival_address_id = Address.query.get(new_arrival_address.id).id
 
         # Crear un nuevo viaje
         new_trip = Trip(
@@ -55,8 +86,8 @@ def create_trip():
             available_seats=data['available_seats'],
             seat_price=data['seat_price'],
             creation_timestamp=now_string,
-            departure_address_id=data['departure_address_id'],
-            arrival_address_id=data['arrival_address_id'],
+            departure_address_id=new_departure_address_id,
+            arrival_address_id=new_arrival_address_id,
             vehicle_driver_id=data['vehicle_driver_id']
         )
 
